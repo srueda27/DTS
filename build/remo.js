8b033010110a5b33081da559518275f01ad64d52
Santiago Rueda Ortiz <srueda27@gmail.com>
2024-03-18 15:46:39 -0500

    p4

--- build/remo.js
+++ build/remo.js
@@ -1,18 +1,30 @@
 function cleanStudentData(student_data) {
+    let missing_data = '';
     // Clean every field from leading or trailing spaces.
-    student_data.last_name = student_data.last_name.trim();
     student_data.first_name = student_data.first_name.trim();
+    if (!student_data.first_name)
+        missing_data += 'First name field is empty. ';
+    student_data.last_name = student_data.last_name.trim();
+    if (!student_data.last_name)
+        missing_data += 'Last name field is empty. ';
     student_data.address = student_data.address.trim();
     student_data.blood_type = student_data.blood_type.trim();
     student_data.phone_number = student_data.phone_number.trim();
     student_data.ID = student_data.ID.trim();
     // Replace more than 1 space with just one space.
-    if (student_data.address)
+    if (student_data.address) {
         student_data.address = student_data.address.replace(/\s{2,}/g, ' ');
+    }
+    else {
+        missing_data += 'Address field is empty. ';
+    }
     // Give the phone number the format of '3 digits - 3 digits - remaining digits'.
     if (student_data.phone_number) {
         const clean_phone_number = student_data.phone_number.replace(/\D/g, '');
         student_data.phone_number = `${clean_phone_number.substring(0, 3)}-${clean_phone_number.substring(3, 6)}-${clean_phone_number.substring(6)}`;
+    }
+    else {
+        missing_data += 'Phone number field is empty. ';
     }
     // Give the blood type the format of letters together and a space for the sign, e.g. 'AB -'.
     if (student_data.blood_type) {
@@ -20,11 +32,19 @@ function cleanStudentData(student_data) {
         const match = student_data.blood_type.match(blood_pattern);
         student_data.blood_type = `${match[1]} ${match[2]}`;
     }
+    else {
+        missing_data += 'Blood type field is empty. ';
+    }
     // Give the ID the format of having a '.' character every 3 digits from the end.
     if (student_data.ID) {
         const clean_ID = student_data.ID.replace(/\D/g, '');
         student_data.ID = clean_ID.split('').reverse().join('').replace(/(\d{3})/g, '$1.').split('').reverse().join('');
     }
+    else {
+        missing_data += 'ID field is empty.';
+    }
+    if (missing_data)
+        student_data.missing_data = missing_data.trim();
     return student_data;
 }
 const student = {

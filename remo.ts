496f1f8324e654f2de862d3afc4bb1cdb9978f8f
Santiago Rueda Ortiz <srueda27@gmail.com>
2024-03-18 15:43:36 -0500

    p2

--- remo.ts
+++ remo.ts
@@ -9,53 +9,35 @@ interface student_data {
 }
 
 function cleanStudentData(student_data: student_data) {
-  let missing_data = ''
-
   // Clean every field from leading or trailing spaces.
-  student_data.first_name = student_data.first_name.trim()
-  if (!student_data.first_name) missing_data += 'First name field is empty. '
-
   student_data.last_name = student_data.last_name.trim()
-  if (!student_data.last_name) missing_data += 'Last name field is empty. '
-
+  student_data.first_name = student_data.first_name.trim()
   student_data.address = student_data.address.trim()
   student_data.blood_type = student_data.blood_type.trim()
   student_data.phone_number = student_data.phone_number.trim()
   student_data.ID = student_data.ID.trim()
 
   // Replace more than 1 space with just one space.
-  if (student_data.address) {
-    student_data.address = student_data.address.replace(/\s{2,}/g, ' ')
-  } else {
-    missing_data += 'Address field is empty. '
-  }
+  if (student_data.address) student_data.address = student_data.address.replace(/\s{2,}/g, ' ')
 
   // Give the phone number the format of '3 digits - 3 digits - remaining digits'.
   if (student_data.phone_number) {
-    const clean_phone_number = student_data.phone_number.replace(/\D/g, '')
-    student_data.phone_number = `${clean_phone_number.substring(0, 3)}-${clean_phone_number.substring(3, 6)}-${clean_phone_number.substring(6)}`
-  } else {
-    missing_data += 'Phone number field is empty. '
+	const clean_phone_number = student_data.phone_number.replace(/\D/g, '')
+	student_data.phone_number = `${clean_phone_number.substring(0, 3)}-${clean_phone_number.substring(3, 6)}-${clean_phone_number.substring(6)}`
   }
 
   // Give the blood type the format of letters together and a space for the sign, e.g. 'AB -'.
   if (student_data.blood_type) {
-    const blood_pattern = /^([A-Z]+)\s*(\+|-)$/;
-    const match = student_data.blood_type.match(blood_pattern)
-    student_data.blood_type = `${match[1]} ${match[2]}`
-  } else {
-    missing_data += 'Blood type field is empty. '
+	const blood_pattern = /^([A-Z]+)\s*(\+|-)$/;
+	const match = student_data.blood_type.match(blood_pattern)
+	student_data.blood_type = `${match[1]} ${match[2]}`
   }
 
   // Give the ID the format of having a '.' character every 3 digits from the end.
   if (student_data.ID) {
-    const clean_ID = student_data.ID.replace(/\D/g, '')
-    student_data.ID = clean_ID.split('').reverse().join('').replace(/(\d{3})/g, '$1.').split('').reverse().join('')
-  } else {
-    missing_data += 'ID field is empty.'
+	const clean_ID = student_data.ID.replace(/\D/g, '')
+	student_data.ID = clean_ID.split('').reverse().join('').replace(/(\d{3})/g, '$1.').split('').reverse().join('')
   }
-
-  if (missing_data) student_data.missing_data = missing_data.trim()
 
   return student_data
 }

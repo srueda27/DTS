92ea19d443c2b9d1d333d813f94e04b810666269
Santiago Rueda Ortiz <srueda27@gmail.com>
2024-03-18 13:31:15 -0500

    Prompt 1

--- remo.ts
+++ remo.ts
@@ -1,154 +1,43 @@
-// Import the file system module to work with files on the computer.
-import * as fs from 'fs';
-
-
-function createCSV(grades: student_info[]) {
-  // Create a constant variable with the name of the file.
-  const filepath = 'course_grades.csv'
-
-  let headers = ['Name']
-
-  // Declare maximum variables to store the maximum quizzes taken each quarter
-  let max_first_quarter = 0
-  let max_second_quarter = 0
-  let max_third_quarter = 0
-  let max_fourth_quarter = 0
-
-  grades.forEach(student => {
-    max_first_quarter = Math.max(max_first_quarter, student.first_quarter.length)
-    max_second_quarter = Math.max(max_second_quarter, student.second_quarter.length)
-    max_third_quarter = Math.max(max_third_quarter, student.third_quarter.length)
-    max_fourth_quarter = Math.max(max_fourth_quarter, student.fourth_quarter.length)
-  })
-
-  // Define an arrow function to create the headers per quarter based on the number of quizzes
-  const createHeader = (quarter: string, qty_quiz: number): string[] => {
-    let headers: string[] = []
-
-    for (let i = 1; i <= qty_quiz; i++) {
-      headers.push(`${quarter}_quiz${i}`)
-    }
-    headers.push(`${quarter}_final`)
-
-    return headers
-  }
-
-  // Create the headers per quarter
-  headers.push(...createHeader('Q1', max_first_quarter))
-  headers.push(...createHeader('Q2', max_second_quarter))
-  headers.push(...createHeader('Q3', max_third_quarter))
-  headers.push(...createHeader('Q4', max_fourth_quarter))
-  headers.push('Final')
-
-  /* 
-    Calculate the final grade per quarter, for each student (without the missing grades). 
-    Round the final grade to only 1 decimal. 
-    Add 0 for every missing grade.
-    Then, add the final grade at the end of the quarter.
-    Calculate the final grade of the course and assign it to the final property of the student.
-  */
-  grades.forEach(student => {
-    let quarter_length = student.first_quarter.length
-    const final_first_quarter = Math.round((student.first_quarter.reduce((a, b) => a + b, 0)) / quarter_length * 10) / 10
-    let missing_grades = max_first_quarter - quarter_length
-    const new_grades = [...student.first_quarter, ...new Array(missing_grades).fill(0), final_first_quarter]
-    student.first_quarter = new_grades
-
-    quarter_length = student.second_quarter.length
-    const final_second_quarter = Math.round((student.second_quarter.reduce((a, b) => a + b, 0)) / quarter_length * 10) / 10
-    missing_grades = max_second_quarter - quarter_length
-    const new_grades_second = [...student.second_quarter, ...new Array(missing_grades).fill(0), final_second_quarter]
-    student.second_quarter = new_grades_second
-
-    quarter_length = student.third_quarter.length
-    const final_third_quarter = Math.round((student.third_quarter.reduce((a, b) => a + b, 0)) / quarter_length * 10) / 10
-    missing_grades = max_third_quarter - quarter_length
-    const new_grades_third = [...student.third_quarter, ...new Array(missing_grades).fill(0), final_third_quarter]
-    student.third_quarter = new_grades_third
-
-    quarter_length = student.fourth_quarter.length
-    const final_fourth_quarter = Math.round((student.fourth_quarter.reduce((a, b) => a + b, 0)) / quarter_length * 10) / 10
-    missing_grades = max_fourth_quarter - quarter_length
-    const new_grades_fourth = [...student.fourth_quarter, ...new Array(missing_grades).fill(0), final_fourth_quarter]
-    student.fourth_quarter = new_grades_fourth
-
-    student.final = Math.round(((final_first_quarter + final_second_quarter + final_third_quarter + final_fourth_quarter) / 4) * 10) / 10
-  })
-
-  // Order the student's grades by final grade in descending order, and then by least quizzes taken.
-  grades.sort((a, b) => {
-    if(a.final != b.final) {
-      return b.final - a.final
-    }
-
-    const total_grades_a = a.first_quarter.filter(num => num != 0).length +
-      a.second_quarter.filter(num => num != 0).length +
-      a.third_quarter.filter(num => num != 0).length +
-      a.fourth_quarter.filter(num => num != 0).length
-
-    const total_grades_b = b.first_quarter.filter(num => num != 0).length +
-      b.second_quarter.filter(num => num != 0).length +
-      b.third_quarter.filter(num => num != 0).length +
-      b.fourth_quarter.filter(num => num != 0).length
-
-    return total_grades_a - total_grades_b
-  })
-
-
-  // Convert the headers to CSV string.
-  const headersString = headers.join(',') + '\n'
-
-  // Convert the list of grades to CSV string. Replace all 0s with N/A
-  const csvContent = grades.map(row =>
-    Object.values(row).join(',')
-  ).join('\n').replace(/,0,/g, ',N/A,');
-
-  console.log('The data has been parsed succesfully.')
-
-  // Write the CSV file, including headers.
-  fs.writeFile(filepath, headersString + csvContent, (err) => {
-    if (err) {
-      console.error('There was an error writing the CSV file:', err);
-    } else {
-      console.log("CSV file has been saved locally with the name 'course_grades.csv'.");
-    }
-  });
+interface student_data {
+  first_name: string,
+  last_name: string,
+  address: string,
+  blood_type: string,
+  ID: string,
+  phone_number: string
 }
 
-// Define an interface with the name and a list of grades per quarter for the student information.
-interface student_info {
-  name: string,
-  first_quarter: number[],
-  second_quarter: number[],
-  third_quarter: number[],
-  fourth_quarter: number[],
-  final?: number
+function cleanStudentData(student_data: student_data) {
+  // Clean every field from leading or trailing spaces.
+  student_data.last_name = student_data.last_name.trim()
+  student_data.first_name = student_data.first_name.trim()
+
+  // Replace more than 1 space with just one space.
+  student_data.address = student_data.address.trim().replace(/\s{2,}/g, ' ')
+
+  // Give the phone number the format of '3 digits - 3 digits - remaining digits'.
+  const clean_phone_number = student_data.phone_number.trim().replace(/\D/g, '')
+  student_data.phone_number = `${clean_phone_number.substring(0, 3)}-${clean_phone_number.substring(3, 6)}-${clean_phone_number.substring(6)}`
+
+  // Give the blood type the format of letters together and a space for the sign, e.g. 'AB -'.
+  const blood_pattern = /^([A-Z]+)\s*(\+|-)$/;
+  const match = student.blood_type.trim().match(blood_pattern)
+  student.blood_type = `${match[1]} ${match[2]}`
+
+  // Give the ID the format of having a '.' character every 3 digits from the end.
+  const clean_ID = student_data.ID.trim().replace(/\D/g, '')
+  student_data.ID = clean_ID.split('').reverse().join('').replace(/(\d{3})/g, '$1.').split('').reverse().join('')
+
+  return student_data
 }
 
-// Declare a dummy data to be replaced later with the actual grades from the database.
-const grades: student_info[] = [
-  {
-    name: 'Charlie',
-    first_quarter: [4.5, 4.8, 3],
-    second_quarter: [4, 3.9, 2.5],
-    third_quarter: [2.8, 3, 4],
-    fourth_quarter: [4.3, 3.7, 2.5]
-  },
-  {
-    name: 'Bob',
-    first_quarter: [2.5, 3.5, 5],
-    second_quarter: [3.7, 4.5, 2.5],
-    third_quarter: [2.2, 3.5, 4.7],
-    fourth_quarter: [3.8, 4.4, 5, 4.9]
-  },
-  {
-    name: 'Alice',
-    first_quarter: [2.5, 3.5, 1, 3],
-    second_quarter: [3.3, 3.5, 4.5],
-    third_quarter: [4, 3.9, 4.4, 3.8],
-    fourth_quarter: [3, 4.5, 5]
-  },
-];
+const student: student_data = {
+  first_name: 'Sebastian ',
+  last_name: 'Perez Silva',
+  address: 'Calle 2 # 1 -  10 ',
+  blood_type: ' AB-',
+  ID: '1165365955',
+  phone_number: '3658859512'
+}
 
-// Example use of the function 
-createCSV(grades);
+console.log(cleanStudentData(student))
